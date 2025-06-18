dbdd53e486e022625986e3996c0f22c5c22313ae
swish0621 <swish0621@gmail.com>
2025-06-18 12:27:44 -0600

    fixed - committed older version

--- Reddit/reddit_requests.py
+++ Reddit/reddit_requests.py
@@ -13,6 +13,7 @@ client_id = os.getenv("REDDIT_CLIENT_ID")
 client_secret = os.getenv("REDDIT_CLIENT_SECRET")
 user_agent = os.getenv("REDDIT_USER_AGENT")
 
+# create reddit instance 
 reddit = praw.Reddit(
     client_id=os.getenv("REDDIT_CLIENT_ID"),
     client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
@@ -22,9 +23,13 @@ reddit = praw.Reddit(
 def search_keyword(word):   
     keyword = word
     data = []
+
+    # if the keyword is listed as a subreddit search that subreddit
     if (subreddit_exists(keyword.replace(" ", ""))): 
         subreddit=reddit.subreddit(keyword.replace(" ", ""))
         for submission in subreddit.hot(limit=5):
+            
+            # add post info to data in json format 
             data.append({
                 "type": "submission",
                 "id": submission.id,
@@ -33,8 +38,11 @@ def search_keyword(word):
                 "score": submission.score,
                 "date": datetime.fromtimestamp(submission.created_utc).isoformat()
             })
+
+            # expand comment tree 
             submission.comments.replace_more(limit=0)
             for comment in list(submission.comments)[:10]:
+                # add comment info to data
                 data.append({
                         "type": "comment",
                         "id": comment.id,
@@ -44,9 +52,13 @@ def search_keyword(word):
                         "date": datetime.fromtimestamp(comment.created_utc).isoformat()
                 })
         return data
+    
+    # if the subreddit doesnt exist check the gaming subreddit
     else:
         for submission in reddit.subreddit("gaming").new(limit=50):
             if keyword.lower() in submission.title.lower():
+
+                # add post info to data in json format 
                 data.append({
                     "type": "submission",
                     "id": submission.id,
@@ -55,8 +67,12 @@ def search_keyword(word):
                     "score": submission.score,
                     "date": datetime.fromtimestamp(submission.created_utc).isoformat()
                 })
+
+                # expand comment tree fully 
                 submission.comments.replace_more(limit=0)
                 for comment in list(submission.comments)[:10]:
+
+                    # add comment info 
                     data.append({
                         "type": "comment",
                         "id": comment.id,
@@ -67,7 +83,8 @@ def search_keyword(word):
                 })
         return data
 
-    
+
+# helper function to check if a subreddit exists
 def subreddit_exists(name):
     try:
         reddit.subreddits.search_by_name(name, exact=True)
